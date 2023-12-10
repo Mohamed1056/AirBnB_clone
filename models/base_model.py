e1040c148cf98080b4faed71108fe3418f2d19c7
Angela-1610 <angelamaged2@gmail.com>
2023-12-10 13:07:02 -0800

    done

---
+++ models/base_model.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+            *args:Unused
+            **kwargs: Key/value
+        """
+
+        time = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                elif k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, time)
+                else:
+                    self.__dict__[k] = v
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
+
+    def __str__(self):
+        """print representation of the BaseModel instance"""
+
+        x = self.__class__.__name__
+        y = self.id
+        z = self.__dict__
+        return "[{}] ({}) {}".format(x, y, z)
+
+
+if __name__ == '__main__':
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}"
+              .format(key, type(my_model_json[key]), my_model_json[key]))
